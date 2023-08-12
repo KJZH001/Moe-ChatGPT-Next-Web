d5a96f9fe4e8db42210d8ea3d25f48eb38f4c8ce
KJZH001 <2802007619@qq.com>
2023-08-12 11:16:19 +0800

    更新 默认基准接口地址

--- next.config.mjs
+++ next.config.mjs
@@ -64,7 +64,7 @@ if (mode !== "export") {
     const ret = [
       {
         source: "/api/proxy/:path*",
-        destination: "https://api.openai.com/:path*",
+        destination: "https://api.moeworld.top/:path*",
       },
       {
         source: "/google-fonts/:path*",
